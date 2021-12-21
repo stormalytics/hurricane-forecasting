09fcc1c2087e1bbc68d6c32f1cb495f49cbe74ba
Stefan Abi-Karam <stefanabikaram@gmail.com>
2021-12-21 14:32:02 -0500

    im a big dummy trying to compare km to n mi, added conversion in distance functions, my errors make much more sense

--- models/demo_v0.py
+++ models/demo_v0.py
@@ -4,6 +4,7 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+from torch._C import _tracer_warn_use_python
 from torch.nn.modules import loss
 from torch.nn.modules.dropout import Dropout
 import tqdm
@@ -99,11 +100,11 @@ class MLP(nn.Module):
             nn.Linear(past_horizon*input_vars_len, hidden_size),
             nn.ReLU(),
             nn.LayerNorm(hidden_size),
-            
+
             Skip(nn.Sequential(nn.Linear(hidden_size, hidden_size),
                                nn.ReLU(),
                                nn.LayerNorm(hidden_size))),
-            
+
             Skip(nn.Sequential(nn.Linear(hidden_size, hidden_size),
                                nn.ReLU(),
                                nn.LayerNorm(hidden_size))),
@@ -125,12 +126,17 @@ class TCN_MLP(nn.Module):
     def __init__(self, input_vars_len: int, target_vars_len: int, past_horizon: int = 1, future_horizon: int = 1, hidden_size=32):
         super(TCN_MLP, self).__init__()
         self.layers = nn.Sequential(
+            nn.BatchNorm1d(input_vars_len),
             TemporalConvNet(past_horizon, [input_vars_len, 16, hidden_size], kernel_size=6, dropout=0.0),
             nn.Flatten(),
 
             nn.Linear(hidden_size*input_vars_len, hidden_size),
             nn.ReLU(),
             nn.LayerNorm(hidden_size),
+
+            Skip(nn.Sequential(nn.Linear(hidden_size, hidden_size),
+                               nn.ReLU(),
+                               nn.LayerNorm(hidden_size))),
 
             Skip(nn.Sequential(nn.Linear(hidden_size, hidden_size),
                                nn.ReLU(),
@@ -160,7 +166,9 @@ def haversine(pred, actual, batch=True):
     alpha = torch.sin((lat_pred-lat_actual)/2)**2 + torch.cos(lat_pred) * \
         torch.cos(lat_actual)*torch.sin((lon_pred-lon_actual)/2)**2
     d = 2*R*torch.arcsin(torch.sqrt(alpha))
-    
+
+    d = d*0.539957
+
     d = d.unsqueeze(-1)
     return d
 
@@ -173,7 +181,7 @@ def equirectangular_approx(pred, actual, batch=True):
     else:
         lon_actual, lon_pred = actual[:, 0], pred[:, 0]
         lat_actual, lat_pred = actual[:, 1], pred[:, 1]
-    
+
     lon_actual, lon_pred = torch.deg2rad(lon_actual), torch.deg2rad(lon_pred)
     lat_actual, lat_pred = torch.deg2rad(lat_actual), torch.deg2rad(lat_pred)
 
@@ -181,18 +189,23 @@ def equirectangular_approx(pred, actual, batch=True):
     y = (lat_pred-lat_pred)
     d = torch.sqrt(x**2 + y**2) * R
 
+    d = d*0.539957
+
     d = d.unsqueeze(-1)
     return d
 
 
-def displacement_seq_to_lat_lon_seq(start_lon_lat, displacement_seq):
-    cumulative_displacement = torch.cumsum(displacement_seq, dim=1)
+# def displacement_seq_to_lat_lon_seq(start_lon_lat, displacement_seq):
+#     cumulative_displacement = torch.cumsum(displacement_seq, dim=1)
+
+
 
 def path_distance_error_location(pred, actual):
     distances = haversine(pred, actual)
     sum_distance = torch.sum(distances, dim=-1)
     average_error = torch.mean(sum_distance)
     return average_error
+
 
 class TSExpLoss:
     def __init__(self, alpha, loss_fn) -> None:
@@ -204,7 +217,7 @@ class TSExpLoss:
 
         exp_weights_t = torch.arange(loss_result.shape[-2], dtype=float, requires_grad=True).to(
             pred.get_device()).unsqueeze(1).repeat(1, loss_result.shape[-1])
-        exp_weights = torch.exp(exp_weights_t/self.alpha)
+        exp_weights = torch.exp(exp_weights_t*self.alpha)/torch.e
 
         loss_result_exp = loss_result * exp_weights
         loss_result_weighted_average = torch.sum(loss_result_exp, dim=[1, 2]) / torch.sum(exp_weights_t)
@@ -216,7 +229,7 @@ class TSExpLoss:
 #         self.alpha = alpha
 #         self.gamma = gamma
 #         self.device = device
-    
+
 #     def __call__(self, pred, actual):
 #         channels = pred.shape[-1]
 #         seq_losses = torch.zeros((channels,))
@@ -229,30 +242,33 @@ class TSExpLoss:
 #         loss = seq_losses.sum()
 #         return loss
 
+
 class SDTWLoss:
     def __init__(self, gamma=0.1, use_cuda=False) -> None:
         self.gamma = gamma
         self.use_cuda = use_cuda
         self.sdtw = SoftDTW(use_cuda=self.use_cuda, gamma=self.gamma)
-    
+
     def __call__(self, pred, actual):
         loss = self.sdtw(pred, actual)
         return loss.mean()
 
+
+
+
 ########################
 ### DEMO STARTS HERE ###
 ########################
-
 
 device = torch.device(torch.cuda.current_device() if torch.cuda.is_available() else 'cpu')
 print(device)
 
 data = pd.read_csv('./data/hurdat/hurdat2_processed.csv')
 
-# data = data[data['year'] == 2015]
 
 input_vars = ["longitude", "latitude", "min_pressure", "max_sus_wind", "landfall", "hour",
-              "jday", "time_idx", "delta_distance", "delta_distance_x", "delta_distance_y", "azimuth"]
+              "jday", "time_idx", "delta_distance", "delta_distance_x", "delta_distance_y", "azimuth",
+              "x", "y", "vpre"]
 output_vars = ["longitude", "latitude"]
 
 
@@ -272,12 +288,10 @@ print(f"# of testing samples: {test_count}")
 train_data, test_data = random_split(
     hurdat_dataset, [train_count, test_count], generator=torch.Generator().manual_seed(42))
 
-batch_size = 512
+batch_size = 256
 train_dataloader = DataLoader(train_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 test_dataloader_viewing = DataLoader(test_data, batch_size=1)
-
-
 
 
 # for sample in hurdat_dataset:
@@ -299,29 +313,31 @@ test_dataloader_viewing = DataLoader(test_data, batch_size=1)
 #     print()
 
 
-
 # exit()
 
 
-
-model = MLP(past_horizon=past_horizon, future_horizon=future_horizon,
-            input_vars_len=12, target_vars_len=2, hidden_size=128)
-# model = TCN_MLP(past_horizon=past_horizon, future_horizon=future_horizon,
-#                 input_vars_len=12, target_vars_len=2, hidden_size=128)
+# model = MLP(past_horizon=past_horizon, future_horizon=future_horizon,
+#             input_vars_len=len(input_vars), target_vars_len=2, hidden_size=128)
+model = TCN_MLP(past_horizon=past_horizon, future_horizon=future_horizon,
+                input_vars_len=len(input_vars), target_vars_len=2, hidden_size=256)
 model = model.to(device)
 print(model)
 
-# loss_fn = nn.L1Loss()
+loss_fn = nn.L1Loss()
 # loss_fn = TSExpLoss(10, loss.L1Loss(reduction='none'))
-# loss_fn = TSExpLoss(10, haversine)
+# loss_fn = TSExpLoss(1/12, haversine)
 # loss_fn = DIALATELoss(device)
-loss_fn = SDTWLoss(use_cuda=False, gamma=0.1)
-optimizer = Adam(model.parameters(), lr=1e-3, weight_decay=1e-2)
+# loss_fn = SDTWLoss(use_cuda=False, gamma=0.1)
+optimizer = Adam(model.parameters(), lr=1e-3, weight_decay=1e-3)
 
 
 def train_loop(dataloader):
     model.train()
-    size = len(dataloader.dataset)
+
+    num_batches = len(dataloader)
+    train_loss = 0
+    train_error_distance = 0
+
     for batch_idx, data in enumerate(dataloader):
         # Compute prediction and loss
         x, y = data["input"].to(device), data["output"].to(device)
@@ -332,14 +348,19 @@ def train_loop(dataloader):
         loss.backward()
         optimizer.step()
 
-        if batch_idx % 50 == 0:
-            loss, current = loss.item(), batch_idx * len(data["input"])
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        train_loss += loss_fn(pred, y).item()
+        train_error_distance += haversine(pred, y, batch=True).mean().detach().item()
+
+    train_loss /= num_batches
+    train_error_distance /= num_batches
+    print(f"Train Loss: {train_loss:>8f}")
+    print(f"Train Error Distance: {train_error_distance:>8f}")
+    print()
 
 
 def test_loop(dataloader):
     model.eval()
-    size = len(dataloader.dataset)
+
     num_batches = len(dataloader)
     test_loss = 0
     test_error_distance = 0
@@ -349,15 +370,18 @@ def test_loop(dataloader):
             x, y = data["input"].to(device), data["output"].to(device)
             pred = model(x)
             test_loss += loss_fn(pred, y).item()
-            test_error_distance += haversine(pred, y, batch=True).mean().detach()
-    
+            test_error_distance += haversine(pred, y, batch=True).mean().detach().item()
+
     test_loss /= num_batches
     test_error_distance /= num_batches
-    print(f"Test Loss: {test_loss:>8f} \n")
-    print(f"Test Error Distance: {test_error_distance:>8f} \n")
+    print(f"Test Loss: {test_loss:>8f}")
+    print(f"Test Error Distance: {test_error_distance:>8f}")
+    print()
 
 
-epochs = 150
+# with torch.autograd.detect_anomaly():
+
+epochs = 200
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader)
@@ -371,16 +395,17 @@ for sample in hurdat_dataset:
     pred = model(x.unsqueeze(0))[0]
     actual = y
     error_dist = haversine(pred, actual, batch=False)
-    error_distances_list.append(error_dist.cpu())
+    error_distances_list.append(error_dist.cpu().detach())
 
 error_distances = torch.stack(error_distances_list)
 error_distances = error_distances[..., 0]
 
-error_distances_df = pd.DataFrame(error_distances.detach().numpy().astype("float"))
+error_distances_df = pd.DataFrame(error_distances.numpy().astype("float"))
 columns = [f'future_horizon_{(i+1)*6}' for i in range(future_horizon)]
 error_distances_df.columns = columns
 print(error_distances_df.describe())
 
 sns.displot(data=error_distances_df, kind="kde")
-plt.xlim(0,1000)
+plt.xlim(0, 400)
+plt.gca().set_xlabel('Forcast Error (n mi)')
 plt.show()
---
+++ models/simple_models.py
@@ -0,0 +1,269 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tqdm
+
+from pathlib import Path
+from typing import List
+from pprint import pprint as pp
+
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler, PolynomialFeatures
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet,\
+    Lars, BayesianRidge, HuberRegressor, RANSACRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.multioutput import MultiOutputRegressor
+
+from sklearn.metrics import mean_absolute_error, make_scorer
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV, train_test_split
+
+import xgboost as xgb
+
+
+class HURDAT(Dataset):
+    def __init__(self, hurdat_table, input_vars: List[str], target_vars: List[str], grouping_var: str, time_idx: str, past_horizon: int = 1, future_horizon: int = 1):
+
+        self.hurdat_table = None
+        if isinstance(hurdat_table, pd.DataFrame):
+            self.hurdat_table = hurdat_table
+        if isinstance(hurdat_table, str):
+            hurdat_table_path = Path(hurdat_table)
+            self.hurdat_table = pd.read_csv(hurdat_table_path)
+
+        self.input_vars = input_vars
+        self.target_vars = target_vars
+
+        self.past_horizon = past_horizon
+        self.future_horizon = future_horizon
+
+        self.grouping_var = grouping_var
+        self.time_idx = time_idx
+
+        self.generated_samples = self.generate_all_ts_samples()
+
+    def generate_all_ts_samples(self) -> list[dict]:
+        all_samples = []
+        for atcf_code, hurricane_df in tqdm.tqdm(self.hurdat_table.groupby(self.grouping_var, sort=False)):
+            storm_samples = self.generate_storm_ts_samples(hurricane_df)
+            all_samples.extend(storm_samples)
+        return all_samples
+
+    def generate_storm_ts_samples(self, storm_df) -> list[dict]:
+        data = []
+        for w in storm_df.rolling(window=self.past_horizon+self.future_horizon):
+            if w.shape[0] == self.past_horizon+self.future_horizon:
+                data_window = {}
+
+                data_window["input"] = torch.tensor(
+                    w.head(self.past_horizon)[self.input_vars].values, dtype=torch.float)
+                data_window["input_time_idx"] = torch.tensor(
+                    w.head(self.past_horizon)[self.time_idx].values, dtype=torch.float)
+
+                data_window["output"] = torch.tensor(w.tail(self.future_horizon)[
+                                                     self.target_vars].values, dtype=torch.float)
+                data_window["output_time_idx"] = torch.tensor(
+                    w.tail(self.future_horizon)[self.time_idx].values, dtype=torch.float)
+
+                data_window["window_time_idx"] = torch.tensor(w[self.time_idx].values, dtype=torch.float)
+                data_window["atcf_code"] = w['atcf_code'].iloc[0]
+
+                data.append(data_window)
+        return data
+
+    def __len__(self):
+        return len(self.generated_samples)
+
+    def __getitem__(self, idx):
+        sample = self.generated_samples[idx]
+        # input_data = sample["input"]
+        # output_data = sample["output"]
+        return sample
+
+
+########################
+### DEMO STARTS HERE ###
+########################
+
+
+data = pd.read_csv('./data/hurdat/hurdat2_processed.csv')
+
+# data = data[data['year'] == 2015]
+
+input_vars = ["longitude", "latitude", "min_pressure", "max_sus_wind", "landfall", "hour",
+              "jday", "time_idx", "delta_distance", "delta_distance_x", "delta_distance_y", "azimuth",
+              "x", "y", "vpre"]
+output_vars = ["longitude", "latitude"]
+
+
+past_horizon = 12
+future_horizon = 8
+hurdat_dataset = HURDAT(data, input_vars=input_vars, target_vars=output_vars,
+                        grouping_var="atcf_code", time_idx="time_idx",
+                        past_horizon=past_horizon, future_horizon=future_horizon)
+
+
+hurdat_dataset_dataloader = DataLoader(hurdat_dataset, batch_size=len(hurdat_dataset))
+
+data = next(iter(hurdat_dataset_dataloader))
+
+x = data["input"].cpu().detach().numpy()
+y = data["output"].cpu().detach().numpy()
+
+print(x.shape)
+print(y.shape)
+
+x = x.reshape((-1, x.shape[-1]*x.shape[-2]))
+y = y.reshape((-1, y.shape[-1]*y.shape[-2]))
+
+print(x.shape)
+print(y.shape)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+
+def haversine(pred, actual, batch=True):
+    R = 6371
+
+    pred = pred.reshape((-1, future_horizon, 2))
+    actual = actual.reshape((-1, future_horizon, 2))
+
+    if batch:
+        lon_actual, lon_pred = actual[:, :, 0], pred[:, :, 0]
+        lat_actual, lat_pred = actual[:, :, 1], pred[:, :, 1]
+    else:
+        lon_actual, lon_pred = actual[:, 0], pred[:, 0]
+        lat_actual, lat_pred = actual[:, 1], pred[:, 1]
+
+    lon_actual, lon_pred = np.deg2rad(lon_actual), np.deg2rad(lon_pred)
+    lat_actual, lat_pred = np.deg2rad(lat_actual), np.deg2rad(lat_pred)
+
+    alpha = np.sin((lat_pred-lat_actual)/2)**2 + np.cos(lat_pred) * \
+        np.cos(lat_actual)*np.sin((lon_pred-lon_actual)/2)**2
+    d = 2*R*np.arcsin(np.sqrt(alpha))
+
+    d = d*0.539957
+
+    return d
+
+
+def haversine_loss(pred, actual, batch=True):
+
+    R = 6371
+
+    pred = pred.reshape((-1, future_horizon, 2))
+    actual = actual.reshape((-1, future_horizon, 2))
+
+    if batch:
+        lon_actual, lon_pred = actual[:, :, 0], pred[:, :, 0]
+        lat_actual, lat_pred = actual[:, :, 1], pred[:, :, 1]
+    else:
+        lon_actual, lon_pred = actual[:, 0], pred[:, 0]
+        lat_actual, lat_pred = actual[:, 1], pred[:, 1]
+
+    lon_actual, lon_pred = np.deg2rad(lon_actual), np.deg2rad(lon_pred)
+    lat_actual, lat_pred = np.deg2rad(lat_actual), np.deg2rad(lat_pred)
+
+    alpha = np.sin((lat_pred-lat_actual)/2)**2 + np.cos(lat_pred) * \
+        np.cos(lat_actual)*np.sin((lon_pred-lon_actual)/2)**2
+    d = 2*R*np.arcsin(np.sqrt(alpha))
+
+    d = d*0.539957
+
+    d_path_sum = np.sum(d, axis=1)
+    d_avg = np.mean(d_path_sum)
+    # d_avg = np.mean(d)
+
+    return d_avg
+
+
+
+pipe = Pipeline([
+    ('scale', RobustScaler()),
+    ('reg',  MultiOutputRegressor(LinearRegression()))
+])
+
+# param_grid = [{'reg__estimator': [SVR(tol=1e-3)],
+#                 'reg__estimator__C': [1, 10, 100],
+#                'reg__estimator__gamma': [1, 0.1, 0.001],
+#                'reg__estimator__degree':[2,3,4],
+#                'reg__estimator__kernel': ['linear', 'rbf', 'poly']}]
+
+# param_grid = [{'reg__estimator': [LinearRegression()]},
+#               {'reg__estimator': [Ridge()],
+#                'reg__estimator__alpha': [0.1, 1.0, 10.0]},
+#               {'reg__estimator': [Lasso(max_iter=10000)],
+#                'reg__estimator__alpha': [0.1, 1.0, 10.0]},
+#               {'reg__estimator': [ElasticNet(max_iter=10000)],
+#                'reg__estimator__alpha': [0.1, 1.0, 10.0],
+#                'reg__estimator__l1_ratio': [0.2, 0.4, 0.6, 0.8]}]
+
+# param_grid = [{'reg__estimator': [DecisionTreeRegressor()],
+#                'reg__estimator__criterion': ['squared_error', 'friedman_mse', 'absolute_error'],
+#                'reg__estimator__min_samples_split': [2, 5, 10],
+#                'reg__estimator__min_samples_leaf': [2, 3, 4, 5, 6, 7, 8]},
+#               ]
+
+# param_grid = [{'reg__estimator': [GradientBoostingRegressor()],
+#                'reg__estimator__loss': ['quantile'],
+#                'reg__estimator__learning_rate': [1, 0.1, 0.01, 0.001],
+#                'reg__estimator__n_estimators': [10, 20, 50, 100, 200, 500],
+#                'reg__estimator__criterion': ['squared_error', 'absolute_error'],
+#                'reg__estimator__max_depth':[3, 4, 5, 6, 7, 8, 9, 10]},
+#               ]
+
+# search = GridSearchCV(pipe, param_grid, cv=5, n_jobs=11, verbose=1,
+#                       scoring=make_scorer(haversine_loss, greater_is_better=False))
+# search.fit(x, y)
+
+# search_results_df = pd.DataFrame({'param': search.cv_results_["params"], 'acc': search.cv_results_["mean_test_score"]})
+# search_results_df.to_csv("grid_search_results.csv", index=False)
+
+
+# y_pred = search.predict(x)
+# error_distances = haversine(y_pred, y)
+
+# error_distances_df = pd.DataFrame(error_distances)
+# columns = [f'future_horizon_{(i+1)*6}' for i in range(future_horizon)]
+# error_distances_df.columns = columns
+# print(error_distances_df.describe())
+
+# sns.displot(data=error_distances_df, kind="kde")
+# plt.xlim(0, 400)
+# plt.gca().set_xlabel('Forcast Error (n mi)')
+# plt.show()
+
+
+
+
+## BOOSTING TREE TEST
+
+reg = MultiOutputRegressor(xgb.XGBRegressor(verbosity=2, n_jobs=12))
+reg.fit(x_train, y_train)
+
+
+y_pred = reg.predict(x_test)
+print(y_pred.shape)
+
+error_distances = haversine(y_pred, y_test)
+print(error_distances.shape)
+
+
+error_distances_df = pd.DataFrame(error_distances)
+columns = [f'future_horizon_{(i+1)*6}' for i in range(future_horizon)]
+error_distances_df.columns = columns
+print(error_distances_df.describe())
+
+sns.displot(data=error_distances_df, kind="kde")
+plt.xlim(0, 400)
+plt.gca().set_xlabel('Forcast Error (n mi)')
+plt.show()
